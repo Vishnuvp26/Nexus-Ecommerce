bff3d8c0453691fb1fdd67c93abdabf106a82355
Vishnu Mohan <vishnumohanmfc@gmail.com>
2025-04-23 12:25:37 +0530

    feat: health checkup

--- app.js
+++ app.js
@@ -40,6 +40,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 // User Route
 app.use('/', userRoute);
 
+//health checkup
+app.get('/health', (req, res) => {
+  res.status(200).send('Healthy')
+})
+
 // Admin Route
 app.use('/admin', adminRoute);
 
